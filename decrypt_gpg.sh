030570cabfaca03274f7335b3a28052020cbd070
rrichards21 <rrichards@inf.udec.cl>
2025-11-27 00:51:38 -0300

    feat: added gpg decrypt for cd

--- decrypt_gpg.sh
+++ decrypt_gpg.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
+# mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
---output $HOME/secrets/keyfile.json keyfile.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" keyfile.json.gpg
